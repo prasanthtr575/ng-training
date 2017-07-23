fee2a93948625c0d639f7b5d83104fdc1af53afe
Prasanth T R <mailprasanthtr@gmail.com>
2017-07-23 15:47:40 +0530

    Http service playground

--- src/app/employee-detail/employee-detail.component.ts
+++ src/app/employee-detail/employee-detail.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data/data.service';
 
 @Component({
-  selector: 'app-employee-detail',
+  selector: 'employee-detail',
   templateUrl: './employee-detail.component.html',
-  styleUrls: ['./employee-detail.component.css']
+  styleUrls: ['./employee-detail.component.css'],
+  providers: [DataService]
 })
 export class EmployeeDetailComponent implements OnInit {
-
-  constructor() { }
+  employees: any[];
+  error: String;
+  constructor(private _data: DataService) {}
 
   ngOnInit() {
+    this._data.getEmployees().subscribe(
+      data => this.employees = data.employees,
+      error => this.error = error
+    );
   }
-
 }
